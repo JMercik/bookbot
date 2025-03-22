5cd095f129f19e2a95c0e736e6ed6f63070a0527
Josh Mercik <josh.mercik@gmail.com>
2025-03-22 16:32:57 -0400

    main.py will output word count of books/frankenstein.txt file to the console

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ def get_book_text():
         text_contents = f.read()
     return text_contents
 
+def word_count():
+    book_text = get_book_text()
+    text_list = book_text.split()
+    count = 0
+
+    for word in text_list:
+        count += 1
+    
+    return count
+
 
 def main():
-    book_text = get_book_text()
-    print(book_text)
+    num_words = word_count()
+    print(f"{num_words} words found in the document")
+    
 
 if __name__ == "__main__":
     main()
