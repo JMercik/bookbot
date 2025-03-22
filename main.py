06103401e0fc8b7ca2d8b55f58df126158ee1270
Josh Mercik <josh.mercik@gmail.com>
2025-03-22 16:31:41 -0400

    main.py will output books/frankenstein.txt file to the console

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+#use title path to return full text
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        text_contents = f.read()
+    return text_contents
+
+
+def main():
+    book_text = get_book_text()
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
+
